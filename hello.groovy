977cd8f6e3ef9efca9f0dc0ca6b52fea7a5636e0
lakshmi bhavani <lakshmi.kommineni5@gmail.com>
2022-02-17 07:27:04 +0800

    Create hello.groovy

---
+++ hello.groovy
@@ -0,0 +1,13 @@
+#!/usr/bin/env groovy
+
+def call(String name = 'human') {
+    echo "Hello, ${name}."
+}
+
+def info(message) {
+   echo "INFO: ${message}"
+}
+
+def warning(message) {
+   echo "WARNING: ${message}"
+}  
